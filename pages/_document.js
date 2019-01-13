b3acdaa0b81bb8d522b866bf4d0875518d7ef7b7
Valentin Gurkov <valentingurkov@gmail.com>
2019-01-13 13:47:30 +0200

    favicons

--- pages/_document.js
+++ pages/_document.js
@@ -13,11 +13,12 @@ export default class MyDocument extends Document {
       <html lang="en">
         <Head>
           <meta key="charset" charSet={DEFAULT_SEO.charset} />
-          <link key="manifest" rel="manifest" href="/_next/static/manifest.json" />
+          <link key="manifest" rel="manifest" type="application/manifest+json" href="/_next/static/manifest.json" />
           <meta name="robots" content="index,follow" />
           <meta key="viewport" name="viewport" content="width=device-width, initial-scale=1" />
           <meta key="msapplication-TileColor" name="msapplication-TileColor" content="#da532c" />
-          <meta key="msapplication-config" name="msapplication-config" content="/static/icons/browserconfig.xml" />
+          <meta name="msapplication-TileImage" content="/static/icons/mstile-144x144.png" />
+          <meta name="msapplication-config" content="/static/icons/browserconfig.xml" />
           <meta key="theme-color" name="theme-color" content="#ff6c0c" />
           <Icons />
           <link href="https://amp.cloudflare.com" rel="preload" as="document" />
@@ -41,11 +42,37 @@ export default class MyDocument extends Document {
 
 const Icons = () => (
   <>
+    <link key="apple-touch-icon" rel="apple-touch-icon" sizes="180x180" href="/static/icons/apple-touch-icon.png" />
     <link
-      key="apple-touch-icon-57x57"
+      key="apple-touch-icon-152x152"
       rel="apple-touch-icon"
-      sizes="57x57"
-      href="/static/icons/apple-touch-icon-57x57.png"
+      sizes="152x152"
+      href="/static/icons/apple-touch-icon-152x152.png"
+    />
+    <link key="icon-32x32" rel="icon" type="image/png" sizes="32x32" href="/static/icons/favicon-32x32.png" />
+    <link key="icon-194x194" rel="icon" type="image/png" sizes="194x194" href="/static/icons/favicon-194x194.png" />
+    <link
+      key="icon-192x192"
+      rel="icon"
+      type="image/png"
+      sizes="192x192"
+      href="/static/icons/android-chrome-192x192.png"
+    />
+    <link key="icon-16x16" rel="icon" type="image/png" sizes="16x16" href="/static/icons/favicon-16x16.png" />
+    <link key="mask-icon" rel="mask-icon" href="/static/icons/safari-pinned-tab.svg" color="#414042" />
+    <link key="shortcut icon" rel="shortcut icon" href="/static/icons/favicon.ico" />{' '}
+    <meta key="msapplication-TileColor" name="msapplication-TileColor" content="#da532c" />
+    <link
+      key="apple-touch-icon-144x144"
+      rel="apple-touch-icon"
+      sizes="144x144"
+      href="/static/icons/apple-touch-icon-144x144.png"
+    />
+    <link
+      key="apple-touch-icon-120x120"
+      rel="apple-touch-icon"
+      sizes="120x120"
+      href="/static/icons/apple-touch-icon-120x120.png"
     />
     <link
       key="apple-touch-icon-60x60"
@@ -72,32 +99,10 @@ const Icons = () => (
       href="/static/icons/apple-touch-icon-114x114.png"
     />
     <link
-      key="apple-touch-icon-120x120"
+      key="apple-touch-icon-57x57"
       rel="apple-touch-icon"
-      sizes="120x120"
-      href="/static/icons/apple-touch-icon-120x120.png"
+      sizes="57x57"
+      href="/static/icons/apple-touch-icon-57x57.png"
     />
-    <link
-      key="apple-touch-icon-144x144"
-      rel="apple-touch-icon"
-      sizes="144x144"
-      href="/static/icons/apple-touch-icon-144x144.png"
-    />
-    <link
-      key="apple-touch-icon-152x152"
-      rel="apple-touch-icon"
-      sizes="152x152"
-      href="/static/icons/apple-touch-icon-152x152.png"
-    />
-    <link
-      key="apple-touch-icon-180x180"
-      rel="apple-touch-icon"
-      sizes="180x180"
-      href="/static/icons/apple-touch-icon-180x180.png"
-    />
-    <link key="icon-32x32" rel="icon" type="image/png" sizes="32x32" href="/static/icons/favicon-32x32.png" />
-    <link key="icon-16x16" rel="icon" type="image/png" sizes="16x16" href="/static/icons/favicon-16x16.png" />
-    <link key="mask-icon" rel="mask-icon" href="/static/icons/safari-pinned-tab.svg" color="#414042" />
-    <link key="shortcut icon" rel="shortcut icon" href="/static/favicon.ico" />
   </>
 );
